a441ae38ba6f83dd636103e85a29a93a13170bc8
layerunk <89783661+layerunk@users.noreply.github.com>
2023-05-05 20:25:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
+
 function setup(){
-    canvas = createCanvas(600 , 400);
-    canvas.position(500, 200);
+    canvas = createCanvas(750 , 450);
+    canvas.position(480, 270);
+    o = ml5.objectDetector("cocossd" , justload);
+    v = createCapture(VIDEO);
+    v.hide();
+}
+
+function justload(){
+    console.log("IT IS LOOADED INITIATION HAS BEGUN");
+}
+
+function draw(){
+    image(v, 0, 0, 750, 450);
 }
